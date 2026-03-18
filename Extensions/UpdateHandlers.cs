08d97395fc466abb0fb4e6d22ba85539474c2a3c
Danil <120283164+DanilSPY36@users.noreply.github.com>
2026-03-18 10:19:14 +0300

    add new updateHandlers and fix updateMatchExtensions

---
+++ Extensions/UpdateHandlers.cs
@@ -0,0 +1,34 @@
+﻿using MAX.Bot.Models.Updates;
+using MAX.Bot.Models.Updates.BotStartStopUpdates;
+using MAX.Bot.Models.Updates.BotUpdates;
+using MAX.Bot.Models.Updates.DialogUpdates;
+using MAX.Bot.Models.Updates.MessageUpdates;
+using MAX.Bot.Models.Updates.UserUpdated;
+using MAX.Bot.Types;
+
+namespace MAX.Bot.Extensions
+{
+    public sealed class UpdateHandlers
+    {
+        public Func<BotStartedUpdate, Task>? BotStarted { get; set; }
+        public Func<BotAddedUpdate, Task>? BotAdded { get; set; }
+        public Func<BotRemovedUpdate, Task>? BotRemoved { get; set; }
+
+        public Func<DialogClearedUpdate, Task>? DialogCleared { get; set; }
+        public Func<DialogMutedUpdate, Task>? DialogMuted { get; set; }
+        public Func<DialogUnmutedUpdate, Task>? DialogUnmuted { get; set; }
+
+        public Func<UserAddedUpdate, Task>? UserAdded { get; set; }
+        public Func<UserRemovedUpdate, Task>? UserRemoved { get; set; }
+
+        public Func<MessageRemovedUpdate, Task>? MessageRemoved { get; set; }
+
+        public Func<ContactInfo, Task>? MessageContact { get; set; }
+        public Func<MessageCreatedUpdate, Task>? MessageCommand { get; set; }
+        public Func<MessageCreatedUpdate, Task>? MessageCreated { get; set; }
+        public Func<MessageEditedUpdate, Task>? MessageEdited { get; set; }
+        public Func<MessageCallbackUpdate, Task>? Callback { get; set; }
+
+        public Func<Update, Task>? Unknown { get; set; }
+    }
+}
--- Extensions/UpdateMatchExtensions.cs
+++ Extensions/UpdateMatchExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using MAX.Bot.Models.Enums;
 using MAX.Bot.Models.Updates;
+using MAX.Bot.Models.Updates.BotStartStopUpdates;
+using MAX.Bot.Models.Updates.BotUpdates;
+using MAX.Bot.Models.Updates.DialogUpdates;
 using MAX.Bot.Models.Updates.MessageUpdates;
+using MAX.Bot.Models.Updates.UserUpdated;
 using MAX.Bot.Types;
 using System;
 using System.Collections.Generic;
@@ -11,34 +15,42 @@ namespace MAX.Bot.Extensions
 {
     public static class UpdateMatchExtensions
     {
-        public static Task Match(this Update update,
-            Func<ContactInfo, Task>? messageContact = null,
-            Func<MessageCreatedUpdate, Task>? messageCommand = null,
-            Func<MessageCreatedUpdate, Task>? messageCreated = null,
-            Func<MessageEditedUpdate, Task>? messageEdited = null,
-            Func<MessageCallbackUpdate, Task>? callback = null,
-            Func<Update, Task>? unknown = null)
+        public static Task Match(this Update update, UpdateHandlers h)
         {
             return update switch
             {
-                MessageCreatedUpdate m when messageContact != null
-                && m.Message.GetContact() is { } contact
-                    => messageContact(contact),
+                BotStartedUpdate b when h.BotStarted != null => h.BotStarted(b),
+                BotAddedUpdate b when h.BotAdded != null => h.BotAdded(b),
+                BotRemovedUpdate b when h.BotRemoved != null => h.BotRemoved(b),
 
-                MessageCreatedUpdate m when messageCommand != null && m.Message.Body?.Text?.StartsWith('/') == true
-                    => messageCommand(m),
+                DialogClearedUpdate d when h.DialogCleared != null => h.DialogCleared(d),
+                DialogMutedUpdate d when h.DialogMuted != null => h.DialogMuted(d),
+                DialogUnmutedUpdate d when h.DialogUnmuted != null => h.DialogUnmuted(d),
 
-                MessageCreatedUpdate m when messageCreated != null
-                    => messageCreated(m),
+                UserAddedUpdate u when h.UserAdded != null => h.UserAdded(u),
+                UserRemovedUpdate u when h.UserRemoved != null => h.UserRemoved(u),
 
-                MessageEditedUpdate m when messageEdited != null
-                    => messageEdited(m),
+                MessageRemovedUpdate m when h.MessageRemoved != null => h.MessageRemoved(m),
 
-                MessageCallbackUpdate c when callback != null
-                    => callback(c),
+                MessageCreatedUpdate m when h.MessageContact != null
+                    && m.Message.GetContact() is { } contact
+                        => h.MessageContact(contact),
 
-                _ when unknown != null
-                    => unknown(update),
+                MessageCreatedUpdate m when h.MessageCommand != null
+                    && m.Message.Body?.Text?.StartsWith('/') == true
+                        => h.MessageCommand(m),
+
+                MessageCreatedUpdate m when h.MessageCreated != null
+                    => h.MessageCreated(m),
+
+                MessageEditedUpdate m when h.MessageEdited != null
+                    => h.MessageEdited(m),
+
+                MessageCallbackUpdate c when h.Callback != null
+                    => h.Callback(c),
+
+                _ when h.Unknown != null
+                    => h.Unknown(update),
 
                 _ => Task.CompletedTask
             };
